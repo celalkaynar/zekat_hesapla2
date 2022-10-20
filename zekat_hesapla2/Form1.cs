e3963dfb9417e4b5d9fe3e685168307697f957d1
Celal Kaynar <celalkaynar7@gmail.com>
2022-10-20 19:09:47 +0300

    Proje dosyası ekle.

---
+++ zekat_hesapla2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zekat_hesapla2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ZekatMiktari;
+            double zekatAltinGr;
+            zekatAltinGr = Convert.ToDouble(textBox1.Text) / 975;
+            if (zekatAltinGr >= 80.18)
+            {
+                ZekatMiktari = (zekatAltinGr / 40) * 975;
+                MessageBox.Show("zekat miktarı" + ZekatMiktari.ToString());
+            }
+            else
+            {
+                MessageBox.Show("zekat vermek için paranız yeterli değil");
+            }
+
+        }
+    }
+}
